832e5d769e335f993e99d59548ab837db2e83593
r-ua <r-ua@i.ua>
2016-04-22 04:59:02 -0700

    add gem simleform  conf cardcontroller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,12 +1,41 @@
 class CardsController < ApplicationController
   def index
-    @cards = Card.all
-  end
-
-  def show
-    @cards = Card.find(params[:id])
+  	@cards = Card.all
   end
 
   def new
+    @card = Card.new
   end
+
+  def create
+    @card = Card.new(card_params)
+    if @card.save
+      redirect_to @card
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  	@card = Card.find(params[:id])
+  end
+
+  def edit
+    @card = Card.find(params[:id])
+  end
+
+  def update
+  	@card = Card.find(params[:id])
+  	if @card.update(card_params)
+  	  redirect_to @card
+  	else
+      render 'edit'
+  	end
+
+  private
+
+  def card_params
+  	params.require(:card).permit(:original_text, :translated_text)
+  end
+
 end
